e3e00177c57255bff60d8c2ebcf17855eaa92208
Alexorcizet <alexorcizet87@gmail.com>
2022-08-02 16:09:50 +0300

    translate object created

---
+++ js/service/i18.book.service.js
@@ -0,0 +1,80 @@
+var gPageTrans = {
+    title: {
+        en: 'The greatest Book Shop',
+        he: 'חנות הספרים הטובה ביותר'
+    },
+    'filter-by': {
+        en: 'filter by:',
+        he: ':סינון על פי'
+    },
+    price: {
+        en: 'Filter by minimum price:',
+        he: 'סינון על פי מחיר מינימלי'
+    },
+    rating: {
+        en: 'Filter by minimum Rating :',
+        he: 'סינון על פי דירוג מינימלי'
+    },
+    'book-title': {
+        en: 'Filter by book title:',
+        he: 'סינון לפי שם ספר'
+    },
+    'sort-by': {
+        en: 'Sort by:',
+        he: 'מיין לפי'
+    },
+
+    'sort-select': {
+        en: 'Sort type',
+        he: 'סוג המיון',
+    },
+    'sort-price': {
+        en: 'Highest price',
+        he: 'מהיקר לזול'
+    },
+    'sort-rating': {
+        en: 'Highest rating',
+        he: 'מהדירוג הגבוה לנמוך'
+    },
+
+
+}
+
+var gTableTrans = {
+    'add-book': {
+        en: 'Add a new Book',
+        he: 'הוסף ספר'
+    },
+    'book-button-read': {
+        en: 'Read',
+        he: 'תקציר'
+    },
+    'book-button-update': {
+        en: 'Update',
+        he: 'עדכון'
+    },
+    'book-button-delete': {
+        en: 'Delete',
+        he: 'מחק'
+    },
+    'header-id': {
+        en: 'Id',
+        he: 'מזהה'
+    },
+    'header-title': {
+        en: 'Title',
+        he: 'שם הספר'
+    },
+    'header-rate': {
+        en: 'Rating',
+        he: 'דירוג'
+    },
+    'header-price': {
+        en: 'Price',
+        he: 'מחיר'
+    },
+    'header-actions': {
+        en: 'Actions',
+        he: 'פעולות'
+    },
+}
